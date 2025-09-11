da657928619532bb3d14fa7414fe541b6df04e15
sai <saiyadamreddy@gmail.com>
2025-09-11 18:43:13 -0500

    features module with Compose screen (FeatureScreen)

--- app/src/main/java/com/app/notesync/MainActivity.kt
+++ app/src/main/java/com/app/notesync/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.app.notesfeature.ui.theme.NotesFeatureScreen
+import com.app.notesfeature.ui.NotesFeatureScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- notesfeature/src/main/java/com/app/notesfeature/ui/NotesFeatureScreen.kt
+++ notesfeature/src/main/java/com/app/notesfeature/ui/NotesFeatureScreen.kt
@@ -1,4 +1,4 @@
-package com.app.notesfeature.ui.theme
+package com.app.notesfeature.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
