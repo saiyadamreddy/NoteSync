24ee7ade5dd22319c019ebe7a99efa82dcf70f47
sai <saiyadamreddy@gmail.com>
2025-09-11 18:13:21 -0500

    Initial commit - NoteSync project setup with Compose

---
+++ app/src/main/java/com/app/notesync/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.app.notesync
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.app.notesfeature.ui.theme.NotesFeatureScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotesFeatureScreen()
+        }
+    }
+}
